9128452965f578b24b63d3783dc580c0646ac74b
deannabosschert <deanna.bosschert@gmail.com>
2021-06-16 12:24:26 +0200

    fix: error

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -17,7 +17,8 @@ const controller = {
             data["likes"] = req.user.likes.filter(
               like => like === req.params.username
             )
-            data["liked"] = profile.likes.filter(like => like === req.user.username)
+            
+            // data["liked"] = profile.likes.filter(like => like === req.user.username)
           }
       
           return res.render("profile", data)
